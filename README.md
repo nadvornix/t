dcddcd5e144d984bff859dbe1d89e6378cafbb2f
Jiří Nádvorník <nadvornik.jiri@gmail.com>
2017-08-05 14:56:11 +0200

    first version

---
+++ README.md
@@ -0,0 +1,12 @@
+
+Small utility for adding tasks to todoist after pressing Win+t.
+
+In I3 add to your config:
+
+    bindsym $mod+t exec /home/jiri/storage/todoist/t
+
+Dependencies:
+https://github.com/Doist/todoist-python
+zenity
+
+
---
+++ t.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+from todoist.api import TodoistAPI
+
+if len(sys.argv) < 2:
+  print("Usage: todoist <msg>")
+  sys.exit()
+
+msg = ' '.join(sys.argv[1:])
+print(msg)
+api = TodoistAPI('244edfbafbfddf12067bdbd866805d928ade76c1')
+print(api.quick.add(msg))
